9d53591463a158c8843bb73278d37a99ac5f45b7
Nils P. Kujath <63961429+NilsKujath@users.noreply.github.com>
2025-02-23 01:20:48 +0100

    Updated run_engine to reflect changes in engine module

--- run_engine.py
+++ run_engine.py
@@ -3,13 +3,11 @@ from time import sleep
 
 engine = TradingEngine(mode=Modes.REPLAY, symbol="MNQZ4")
 
-
-bar_1 = engine.data_handler.get_next_bar()
-bar_2 = engine.data_handler.get_next_bar()
-print(bar_1)
-print(bar_2)
 engine.add_indicator(SimpleMovingAverage(period=5, applied_on="close"))
 engine.add_indicator(SimpleMovingAverage(period=10, applied_on="close"))
+
 engine.connect()
+
 sleep(0.1)
 engine.stop()
+
